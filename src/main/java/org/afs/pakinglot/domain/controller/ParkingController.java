f10c18030bd1e49e706f7d82f828e39daea82e30
HEDO <hedo@oocl.com>
2024-12-04 17:05:31 +0800

    feat: add getParkingStrategy api

---
+++ src/main/java/org/afs/pakinglot/domain/controller/ParkingController.java
@@ -0,0 +1,27 @@
+package org.afs.pakinglot.domain.controller;
+
+import org.afs.pakinglot.domain.entity.Car;
+import org.afs.pakinglot.domain.entity.ParkingLot;
+import org.afs.pakinglot.domain.entity.Ticket;
+import org.afs.pakinglot.domain.service.ParkingService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/parking")
+public class ParkingController {
+
+    private ParkingService parkingService;
+
+    public ParkingController(ParkingService parkingService) {
+        this.parkingService = parkingService;
+    }
+
+    @GetMapping("/strategy")
+    public List<String> getParkingStrategy() {
+        return parkingService.getParkingStrategy();
+    }
+
+
+}
---
+++ src/main/java/org/afs/pakinglot/domain/service/ParkingService.java
@@ -0,0 +1,30 @@
+package org.afs.pakinglot.domain.service;
+
+import org.afs.pakinglot.domain.entity.Car;
+import org.afs.pakinglot.domain.ParkingBoy;
+import org.afs.pakinglot.domain.entity.ParkingLot;
+import org.afs.pakinglot.domain.entity.Ticket;
+import org.afs.pakinglot.domain.strategies.ParkingStrategy;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class ParkingService {
+
+    private Map<String, ParkingStrategy> strategyMap;
+
+    private ParkingBoy parkingBoy;
+
+    public ParkingService(Map<String, ParkingStrategy> strategyMap, ParkingBoy parkingBoy) {
+        this.strategyMap = strategyMap;
+        this.parkingBoy = parkingBoy;
+    }
+
+    public List<String> getParkingStrategy() {
+        return strategyMap.keySet().stream().toList();
+    }
+
+
+}
